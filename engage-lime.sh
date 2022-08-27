90685c0b3bcba601180cdc2331d7afbc0c0690f8
synthetic-intelligence <69837543+synthetic-intelligence@users.noreply.github.com>
2022-08-27 02:42:36 -0600

    Update engage-lime.sh

--- engage-lime.sh
+++ engage-lime.sh
@@ -3,13 +3,14 @@
 # git clone https://github.com/synthetic-intelligence/dots.com.git .prefs
 cd .prefs
 
-chsh -s /bin/zsh
-exec su -l $USER
-
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 curl -L git.io/antigen > ~/.antigen.zsh
 
 cp -R .aliases .antigenrc .local .zshrc .vimrc .choices ~/
+
+
+chsh -s /bin/zsh
+exec su -l $USER
 
 source ~/.zshrc
 
