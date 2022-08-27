9aabb51b294041488d7273946073f8024fb089de
Your Name <you@example.com>
2022-08-27 17:53:40 -0600

    readding helper script

--- engage-ubuntu.sh
+++ engage-ubuntu.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+PATH_DIR="$HOME/.local/bin"
+mkdir -p "$PATH_DIR"
+curl https://cht.sh/:cht.sh > "$PATH_DIR/cht.sh"
+chmod +x "$PATH_DIR/cht.sh"
 
     
     sudo apt install zsh bat xsel tree neofetch autojump tldr vim wget git bpytop exa htop
